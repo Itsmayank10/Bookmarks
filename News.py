584c8b7d6e3e2f4d522b4501266c9450eeb2cadb
Itsmayank10 <itsmayank10@gmail.com>
2020-04-05 18:59:19 +0530

    Initial commit

---
+++ News.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+def speak(str):
+    from win32com.client import Dispatch
+    speak = Dispatch("SAPI.SpVoice")
+    speak.Speak(str)
+
+if __name__ == '__main__':
+    speak("News for today.. Lets begin")
+    url = "http://newsapi.org/v2/top-headlines?country=in&apiKey=24c025a89dcb4423979f9b58c94a1d70"
+    news = requests.get(url).text
+    news_dict = json.loads(news)
+    arts = news_dict['articles']
+    for article in arts:
+        speak(article['title'])
+        print(article['title'])
+        speak("Agli Khabar")
+
+    speak("Thanks for listening...")
